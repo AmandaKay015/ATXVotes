57df6fd803b63eae0ff102538b9e7705f153b99b
smhigley <sarahmhigley@gmail.com>
2014-06-11 20:26:32 -0500

    added candidate filtering by position

--- public/app/scripts/views/elections_view.js
+++ public/app/scripts/views/elections_view.js
@@ -1,2 +1,19 @@
 App.ElectionsView = Ember.View.extend({
+  candidateContainer: null,
+  filterButtons: null,
+  actions: {
+    setFilterClass: function (className) {
+      // store candidate container and filter button variable when action is first called
+      if(!this.candidateContainer || !this.filterButtons) {
+        console.log("candidateContainer not defined");
+        this.candidateContainer = this.$().find('.candidate-list');
+        this.filterButtons = this.$().find('.filter-btn');
+      }
+
+      // set filter button classes
+      this.filterButtons.removeClass('active').filter('.' + className).addClass('active');
+
+      this.candidateContainer.removeClass('mayor all council').addClass(className);
+    }
+  }
 });
