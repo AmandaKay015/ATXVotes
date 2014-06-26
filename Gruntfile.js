b8db4eb6a1c5e020243fe7b6dc7d907d3229e1dd
smhigley <sarahmhigley@gmail.com>
2014-06-25 20:20:02 -0500

    updated grunt to build to public_html

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
     // configurable paths
     var yeomanConfig = {
         app: 'app',
-        dist: 'dist'
+        dist: 'public_html'
     };
 
     grunt.initConfig({
